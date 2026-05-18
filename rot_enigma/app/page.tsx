dbd94a469ed18b749ff360dabf47b66df510d084
SonponYe-shuaChief <sonponyeshua@gmail.com>
2026-05-18 16:46:00 +0000

    added to normal page

--- rot_enigma/app/page.tsx
+++ rot_enigma/app/page.tsx
@@ -1,9 +1,21 @@
 import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "./page.module.css";
+import Footer from "./components/footer";
+import OutputField from "./components/output";
+import { button } from "./components/button";
+import normal from "./pages/normal";
+import pages from "next/dist/build/templates/pages";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
-    <p>Soon to come..</p>
+    
+ 
+    {button()}
+    <Footer />
     </>
   );
 }
--- rot_enigma/app/pages/normal.jsx
+++ rot_enigma/app/pages/normal.jsx
@@ -1,3 +1,17 @@
 import button from '../components/button';
 import output_field from '../components/output';
 import footer from '../components/footer';
+
+function normal () {
+    return (
+        <div className="normal-page">
+            <h1>ROT_Enigma</h1>
+            <p>HERE IT IS ...</p>
+            {button()}
+            {output_field()}
+            {footer()}
+        </div>
+    )
+}
+
+export default normal;
