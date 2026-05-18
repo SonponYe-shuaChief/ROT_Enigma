092f60d930fb6f95a728b710deb4fcee0e1c7546
SonponYe-shuaChief <sonponyeshua@gmail.com>
2026-05-18 13:40:09 +0000

    just to add to commis

---
+++ rot_enigma/app/components/footer.jsx
@@ -0,0 +1,8 @@
+function footer ()  {
+    return (
+        <footer className="footer">
+            <p>Copyright &copy; 2024 ROT_Enigma. All rights reserved.</p>
+            <p>Sonpon Ye-shua Chief</p>
+        </footer>
+    )
+}
